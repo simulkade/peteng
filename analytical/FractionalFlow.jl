f68414ae4a4db193b23804267c86e72a19b5d37c
Ali A Eftekhari <e.eftekhari@gmail.com>
2018-05-04 00:34:50 +0200

    the problematic case

--- analytical/FractionalFlow.jl
+++ analytical/FractionalFlow.jl
@@ -235,7 +235,7 @@ function tangent_line_saturation(rel_perms, fluids, sw_fw_point)
     end
 end
 
-function cross_point_saturation(fw, rel_perms, point1, point2)
+function cross_point_saturation(fw, rel_perms, point1, point2; sw_left=0, sw_right=0)
     # create the equation for line
     sor = rel_perms.sor
     swc = rel_perms.swc
@@ -246,7 +246,9 @@ function cross_point_saturation(fw, rel_perms, point1, point2)
     f_tmp = f.(sw_tmp)
     sw_neg = sw_tmp[indmin(f_tmp)]
     sw_pos = sw_tmp[indmax(f_tmp)]
-    sw_left, sw_right = minmax(sw_neg, sw_pos)
+    if sw_right==0
+        sw_left, sw_right = minmax(sw_neg, sw_pos)
+    end
     sw_cross = sw_tmp[indmin(abs.(f_tmp))]
     try
         if f(sw_left)*f(sw_right)>0
@@ -342,7 +344,12 @@ function single_ion_adsorption_water_flood(core_props, fluids_ls, fluids_hs, rel
     sw_init = core_flood.initial_water_saturation    
     t_D_BT_hs = (sw_shock_hs-sw_init)/(fw_hs(sw_shock_hs)-fw_hs(sw_init)) # breakthrough (BT) time [#PV]
     println("high sal breakthrough time = $t_D_BT_hs")
-    
+
+    # check the slope of the lines:
+    if t_D_BT_hs>=t_D_BT_ls
+        info("This function does not work on this problem. Consider calling the single_ion_adsorption_water_flood_single_shock function.")
+    end
+
     # tracer
     sw_tracer = tangent_line_saturation(rel_perms_ls, fluids_ls, (0.0, 0.0))
     t_D_tracer = 1/dfw_ls(sw_tracer)
@@ -392,7 +399,7 @@ function single_ion_adsorption_water_flood(core_props, fluids_ls, fluids_hs, rel
                             [pv_R R], [pv_R*pv_to_t R], [xt_prf sw_prf], [xt_tracer c_tracer])
 end
 
-function single_ion_adsorption_tertiary_water_flood(core_props, fluids_ls, fluids_hs, rel_perms_hs, 
+function single_ion_adsorption_water_flood_single_shock(core_props, fluids_ls, fluids_hs, rel_perms_hs, 
     rel_perms_ls, core_flood, eq_const)
     # ====================================================================
     # This function is not done yet. The trick is to draw a tangent from 
@@ -411,25 +418,34 @@ function single_ion_adsorption_tertiary_water_flood(core_props, fluids_ls, fluid
     println("low sal sw_shock = $sw_shock_ls")
     t_D_BT_ls = 1/dfw_ls(sw_shock_ls) # breakthrough (BT) time [#PV]
     println("low sal breakthrough time = $t_D_BT_ls")
-    # This is speculation: sometimes the calculated low sal shock sw is lower than 1-sor_ls
-    # which is impossible. Therefore, I correct it here, and also correct the low sal
-    # breakthrough time. Therefore, the following correction is applied:
-    sor_hs = rel_perms_hs.sor
-    if (sw_shock_ls<1-sor_hs)
-        info("the calculated shock front $sw_shock_ls is less than $(1-sor_hs). Correection is needed")
-        sw_shock_ls = tangent_line_saturation(rel_perms_ls, fluids_ls, (1-sor_hs, 0.0))
-        println("Corrected: low sal sw_shock = $sw_shock_ls")
-        t_D_BT_ls = dfw_ls(sw_shock_ls)
-        info("low sal breakthrough time corrected to $t_D_BT_ls_w")
-    end
-    
     # High sal shock (cross point between the ls tangent and the hs fw)
     sw_shock_hs = cross_point_saturation(fw_hs, rel_perms_hs, (-eq_const, 0.0), (sw_shock_ls, fw_ls(sw_shock_ls)))
     println("high sal sw_shock = $sw_shock_hs")
-    t_D_BT_hs = (sw_shock_hs-(1-sor_hs))/(fw_hs(sw_shock_hs)-fw_hs(1-sor_hs)) # breakthrough (BT) time [#PV]
+    sw_init = core_flood.initial_water_saturation    
+    t_D_BT_hs = (sw_shock_hs-sw_init)/(fw_hs(sw_shock_hs)-fw_hs(sw_init)) # breakthrough (BT) time [#PV]
     println("high sal breakthrough time = $t_D_BT_hs")
-    
-    
+    if t_D_BT_hs<t_D_BT_ls
+        error("Use the single_ion_adsorption_water_flood function.")
+    end
+    sor_ls = rel_perms_ls.sor
+    sw_shock = cross_point_saturation(fw_ls, rel_perms_ls, (sw_shock_hs, fw_hs(sw_shock_hs)), 
+        (sw_init, fw_hs(sw_init)), sw_left = sw_shock_ls, sw_right = 1-sor_ls)
+    if sw_shock<sw_shock_ls
+        sw_shock = cross_point_saturation(fw_ls, rel_perms_ls, (sw_shock, fw_ls(sw_shock)), 
+            (sw_init, fw_hs(sw_init)), sw_left = sw_shock_ls, sw_right = 1-sor_ls)
+    end
+    sor_hs = rel_perms_hs.sor
+    t_D_BT_hs = (sw_shock-sw_init)/(fw_ls(sw_shock)-fw_hs(sw_init))
+    println("sw_shock = $sw_shock")
+    println("high sal breakthrough time = $t_D_BT_hs")
+    t_D_BT_ls = 1/dfw_ls(sw_shock)
+    println("low sal breakthrough time = $t_D_BT_ls")
+
+    # tracer
+    sw_tracer = tangent_line_saturation(rel_perms_ls, fluids_ls, (0.0, 0.0))
+    sw_tracer = max(sw_tracer, sw_shock)
+    t_D_tracer = 1/dfw_ls(sw_tracer)
+    xt_tracer_shock = dfw_ls(sw_tracer)
 
     # construct the recovery factor curve versus the # of PV
     R = zeros(1)
@@ -437,36 +453,42 @@ function single_ion_adsorption_tertiary_water_flood(core_props, fluids_ls, fluid
     # at breakthrough of the hs brine
     sw_init = core_flood.initial_water_saturation
     push!(R, 0.0) # recovery at BT
-    push!(pv_R, t_D_BT_hs) # BT time
+    push!(pv_R, t_D_BT_ls) # BT time
     # at breakthrough of the ls brine
-    push!(R, (1-fw_hs(sw_shock_hs))*(t_D_BT_ls-t_D_BT_hs)/(1-sw_init)) # recovery at BT
+    push!(R, (1-fw_hs(sw_shock))*(t_D_BT_ls-t_D_BT_hs)/(1-sw_init)) # recovery at BT
     push!(pv_R, t_D_BT_ls) # BT time
 
     # after breakthrough
     pv_inj = max(core_flood.injected_pore_volume, 2.0) # at least inject 2 pv
     f_sw = sw -> (pv_inj-1/dfw_ls(sw)) # find the outlet saturation at the end of injection
-    sw_max = fzero(f_sw, sw_shock_ls)
-    sw_tmp = linspace(sw_shock_ls, sw_max, 100)
+    sw_max = fzero(f_sw, sw_shock)
+    println("swmax $sw_max")
+    sw_tmp = linspace(sw_shock, sw_max, 100)
     t_D_tmp = 1./dfw_ls.(sw_tmp)
     
     s_av_tmp = sw_tmp-(fw_ls.(sw_tmp)-1).*t_D_tmp
-    R_tmp = (s_av_tmp-(1-sor_hs))/(1-sw_init)
+    R_tmp = (s_av_tmp-sw_init)/(1-sw_init)    
     append!(R, R_tmp)
     append!(pv_R, t_D_tmp)
 
     # saturation profile
-    sor_ls = rel_perms_ls.sor
     sw_inj = core_flood.injected_water_saturation
     phi = core_props.porosity
     ut = core_flood.injection_velocity
-    s1 = collect(linspace(min(sw_inj, 1-sor_ls-eps()), sw_shock_ls-eps(), 100))
+    L = core_props.length
+    pv_to_t = phi*L/ut
+    s1 = collect(linspace(min(sw_inj, 1-sor_ls-eps()), sw_shock-eps(), 100))
     xt_s1 = dfw_ls.(s1)
     xt_shock_ls = 1/t_D_BT_ls
     xt_shock_hs = 1/t_D_BT_hs
     xt_prf=[xt_s1; xt_shock_ls; xt_shock_ls+eps(); xt_shock_hs; xt_shock_hs+eps(); 1/0.3]
-    sw_prf=[s1; sw_shock_ls; sw_shock_hs; sw_shock_hs; 1-sor_hs; 1-sor_hs]
-
-    return pv_R, R, xt_prf, sw_prf # for the time being to test the code
+    sw_prf=[s1; sw_shock; sw_shock; sw_shock; sw_init; sw_init]
+    xt_tracer = [0.0, xt_tracer_shock, xt_tracer_shock+eps(), xt_prf[end]]
+    c_tracer = [1.0, 1.0, 0.0, 0.0]
+    return FracFlowResults([fw_hs, fw_ls], [Line([-eq_const, 0.0], [sw_shock_ls, fw_ls(sw_shock_ls)]),
+                            Line([sw_init, fw_hs(sw_init)], [sw_shock_hs, fw_hs(sw_shock_hs)])], 
+                            [pv_R R], [pv_R*pv_to_t R], [xt_prf sw_prf], [xt_tracer c_tracer])
+    # return pv_R, R, xt_prf, sw_prf # for the time being to test the code
 end
 
 """
